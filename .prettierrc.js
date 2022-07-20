7eadde7cc4e8f821c8ee06502f3a5ea3ebb1cf67
Nick Levshin <nikita.levshin2016@gmail.com>
2022-07-20 12:50:29 +0300

    feat: add deploy link

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'es5',
-  singleQuote: true,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  tabWidth: 2,
-  endOfLine: 'auto',
-}
---
+++ REAME.md
@@ -0,0 +1,1 @@
+deploy: https://nick-levshin.github.io/online-store
